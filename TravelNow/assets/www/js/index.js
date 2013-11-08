2523dadee611f4c4268384129b2f290788ce2103
AmaHack <hackathon.amadeus@gmail.com>
2013-11-08 20:26:07 +0530

    Added maps

--- TravelNow/assets/www/js/index.js
+++ TravelNow/assets/www/js/index.js
@@ -45,5 +45,16 @@ var app = {
         receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);
+    },
+    mapinitialize : function () {
+    	var mapOptions = {
+    		    zoom: 8,
+    		    center: new google.maps.LatLng(-34.397, 150.644),
+    		    mapTypeId: google.maps.MapTypeId.ROADMAP
+    		  };
+    		  map = new google.maps.Map(document.getElementById('map-canvas'),
+    		      mapOptions);
+
+    }
     }
 };
